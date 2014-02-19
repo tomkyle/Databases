35eb1f4cf1d19f8f882cd6e7ed84cde4f30b9fb0
Carsten Witt <carsten.witt@gmail.com>
2014-02-19 19:05:48 +0100

    Added first mock-test

---
+++ tests/DatabaseServiceLocatorTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace tests;
+
+class DatabaseServiceLocatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
