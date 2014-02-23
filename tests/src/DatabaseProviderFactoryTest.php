6650bfbc834dd053cd7584046ee95144928a2c23
Carsten Witt <carsten.witt@gmail.com>
2014-02-23 17:41:34 +0100

    New Test for DatabaseProviderFactory

---
+++ tests/src/DatabaseProviderFactoryTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace tests;
+
+use \tomkyle\Databases\DatabaseProvider;
+use \tomkyle\Databases\DatabaseProviderFactory;
+use \tomkyle\Databases\DatabaseConfig;
+
+class DatabaseProviderFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Provides valid arguments to DatabaseProviderFactory factory method.
+     *
+     * @dataProvider provideValidCtorArguments
+     */
+    public function testValidProviderByFactoryMethod($valid)
+    {
+        $provider = new DatabaseProviderFactory;
+        $this->assertInstanceOf(
+            '\tomkyle\Databases\DatabaseProvider', $provider->newInstance($valid));
+    }
+
+    /**
+     * Returns a set of valid ctor arguments.
+     */
+    public function provideValidCtorArguments()
+    {
+        return array(
+            array( new DatabaseConfig( $this->createArrayConfigArgument() )),
+            array( new DatabaseConfig( $this->createStdClassConfigArgument() ))
+        );
+    }
+
+    /**
+     * Creates a valid ctor array argument.
+     *
+     * @return array
+     */
+    protected function createArrayConfigArgument()
+    {
+        return array(
+                'host' =>     "localhost",
+                'database' => "database1",
+                'user' =>     "root",
+                'pass' =>     "secret",
+                'charset' =>  "utf8"
+            );
+    }
+
+    /**
+     * Creates a valid StdClass ctor argument.
+     *
+     * @return \StdClass
+     */
+    protected function createStdClassConfigArgument()
+    {
+        return json_decode('{
+                "host":     "localhost",
+                "database": "database1",
+                "user":     "root",
+                "pass":     "secret",
+                "charset":  "utf8"
+            }');
+    }
+
+}
+
