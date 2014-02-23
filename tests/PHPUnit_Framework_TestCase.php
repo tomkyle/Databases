14fad4d0382692b0c2175361e549f7cb32ccf491
Carsten Witt <carsten.witt@gmail.com>
2014-02-23 20:52:36 +0100

    New TestCase base class

---
+++ tests/PHPUnit_Framework_TestCase.php
@@ -0,0 +1,45 @@
+<?php
+namespace tomkyle;
+
+
+class PHPUnit_Framework_TestCase extends \PHPUnit_Framework_TestCase
+{
+
+
+    /**
+     * Checks if Test Suite is run on Travis CI.
+     *
+     * @return boolean
+     */
+    public function isTravisCi()
+    {
+        return (isset($_SERVER['ci'])
+            and isset($_SERVER['TRAVIS'])
+            and $_SERVER['CI']     == "true"
+            and $_SERVER['TRAVIS'] == "true");
+    }
+
+
+
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     *
+     * @see  https://jtreminio.com/2013/03/unit-testing-tutorial-part-3-testing-protected-private-methods-coverage-reports-and-crap/
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+}
